26770a3887619ee7d319b9a9358be672aa144ba9
Marco Loiacono <marcoloiacono034@gmail.com>
2026-02-23 16:42:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2152291626-61ba0",
+  "appId": "1:447609623779:web:022867ebe3ec8baa21e1d6",
+  "apiKey": "AIzaSyBSWVlVuwZNdtZahu3tw-dOJwkl9hR-l_w",
+  "authDomain": "studio-2152291626-61ba0.firebaseapp.com",
+  "measurementId": "G-NXVGF1NZ6Q",
+  "messagingSenderId": "447609623779"
+};
